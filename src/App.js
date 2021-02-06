fd1be2c87e22afd4519bb55fd6ea2706d4047ccb
Rao Fahad <raofahad046@gmail.com>
2021-02-06 13:01:51 -0800

    added header and dropdown functionality and fetch data from api with useEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,49 @@
 import "./App.css";
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import { MenuItem, FormControl, Select } from "@material-ui/core";
 function App() {
-	return <div className='app'></div>;
+	const [country, setCountry] = useState("worldwide");
+	const [countries, setCountries] = useState([]);
+
+	useEffect(() => {
+		const getCountriesData = async () => {
+			await fetch("https://disease.sh/v3/covid-19/countries")
+				.then((response) => response.json())
+				.then((data) => {
+					const countries = data.map((country) => ({
+						name: country.country,
+						value: country.countryInfo.iso2,
+					}));
+					setCountries(countries);
+				});
+		};
+
+		getCountriesData();
+	}, []);
+
+	const onCountryChange = (event) => {
+		const countryCode = event.target.value;
+
+		console.log(countryCode);
+
+		setCountry(countryCode);
+	};
+
+	return (
+		<div className="app">
+			<div class="app__header">
+				<h1>COVID-19 TRACKER</h1>
+				<FormControl class="app__dropdown">
+					<Select variant="outlined" onChange={onCountryChange} value={country}>
+						<MenuItem value="worldwide">Worldwide</MenuItem>
+						{countries.map((country) => (
+							<MenuItem value={country.value}>{country.name}</MenuItem>
+						))}
+					</Select>
+				</FormControl>
+			</div>
+		</div>
+	);
 }
 
 export default App;
